15f8e0df720ea846cdb1f4b0eb070690752e2ec3
SarangS17 <sarang17091709@gmail.com>
2025-11-28 14:11:49 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,15 @@
+import json
+import os
 
+DB_FILE = "library_data.json"
+
+def load_data():
+    if not os.path.exists(DB_FILE):
+        return {"books": [], "students": [], "issued": []}
+
+    with open(DB_FILE, "r") as file:
+        return json.load(file)
+
+def save_data(data):
+    with open(DB_FILE, "w") as file:
+        json.dump(data, file, indent=4)
