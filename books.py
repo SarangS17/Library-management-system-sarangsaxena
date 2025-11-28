008f502c1347ba85fb01dc6243db7536dec24466
SarangS17 <sarang17091709@gmail.com>
2025-11-28 14:12:54 +0530

    Update books.py

--- books.py
+++ books.py
@@ -1,1 +1,18 @@
+from database import load_data, save_data
 
+def add_book():
+    data = load_data()
+    title = input("Enter book title: ")
+    author = input("Enter author name: ")
+    book_id = input("Enter book ID: ")
+
+    data["books"].append({"title": title, "author": author, "book_id": book_id})
+    save_data(data)
+    print("Book added successfully\n")
+
+def view_books():
+    data = load_data()
+    print("\n---- Available Books ----")
+    for book in data["books"]:
+        print(f"{book['book_id']} - {book['title']} by {book['author']}")
+    print()
